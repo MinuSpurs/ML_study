b86eb0f0ff7ab757a0d8bf51beecefeb17b5c607
오민우 <minwoo@omin-uui-MacBookAir.local>
2024-01-31 16:34:27 +0900

    finished

--- coder.py
+++ coder.py
@@ -7,6 +7,23 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.content, 'html.parser',)
 
-jobs = soup.find('section', class_='jobs').find_all('li')
+jobs = soup.find('section', class_='jobs').find_all('li')[1:-1]
 
-print(jobs)
+all_jobs = []
+
+for job in jobs:
+    title = job.find('span', class_='title').text
+    company, position, region = job.find_all('span', class_='company')
+    url = job.find('div', class_='tooltip').next_sibling
+    if url:
+        url = url['href']
+    job_data = {
+        'title': title,
+        'company': position.text,
+        'position': position.text,
+        'region': region.text,
+        'url': f'https://weworkremotely.com{url}'
+    }
+    all_jobs.append(job_data)
+
+print(all_jobs)
