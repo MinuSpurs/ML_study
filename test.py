9b33acdc62fdebd3239a66d357d5178f6e47affa
Minwoo <gapbu123@nate.com>
2024-02-01 09:32:29 +0900

    finished

--- test.py
+++ test.py
@@ -3,12 +3,6 @@ from bs4 import BeautifulSoup
 
 all_jobs = []
 
-response = requests.get(
-    "https://berlinstartupjobs.com/engineering/",
-    headers={
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-    }
-)
 
 skills = [
     "python",
@@ -19,39 +13,54 @@ skills = [
 
 def scrape_page(url):
     print(f'Scrapping {url}...')
-    response = requests.get(url)
+    response = requests.get(
+        url,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        }
+    )
 
     soup = BeautifulSoup(response.content, 'html.parser',)
-
+    
     jobs = soup.find('ul', class_='jobs-list-items').find_all('li')
-
+    
     for job in jobs:
-        title = job.find('span', class_='title').text
-        company, position = job.find_all('span', class_='company')
-        url = job.find('div', class_='tooltip').next_sibling
-        url = job.find('div', class_='a')
-        if url:
-            url = url['href']
+        position = job.find('div', class_='bjs-jlid__meta').find('a', class_='bjs-jlid__b').text
+        title = job.find('div', class_='bjs-jlid__meta').find('h4', class_='bjs-jlid__h').text
+        link = job.find('div', class_='bjs-jlid__meta').find('a')
+        if link:
+            link = link['href']        
         job_data = {
             'title': title,
-            'company': company.text,
-            'position': position.text,
-            'url': f'{url}'
+            'position': position,
+            'link': link
         }
         all_jobs.append(job_data)
-'''
+
+    
+
+
 def get_pages(url):
-    response = requests.get(url)
+    response = requests.get(url, headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    })
 
     soup = BeautifulSoup(response.content, 'html.parser',)
 
-    return len(soup.find_all('div', class_='widget popular_skills').find_all('li', class_='link'))
+    return len((soup.find('ul', class_='bsj-nav').text)[1:3])
 
-url= 'https://berlinstartupjobs.com/engineering/'
-total_page = get_pages(url)'''
+url = "https://berlinstartupjobs.com/engineering/"
 
+total_page = get_pages(url)
+
+for x in range(total_page):
+    url = f'https://berlinstartupjobs.com/engineering/page/{x+1}/'
+    print(url)
+    
+url = ''
+    
 for skill in skills:
-    url = f'https://berlinstartupjobs.com/skill-areas/python/{skill}'
+    url = f'https://berlinstartupjobs.com/skill-areas/{skill}/'
     scrape_page(url)
 
 print(len(all_jobs))
